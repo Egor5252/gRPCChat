9eeff47737d4af66a2796ce893e77cc017ab2cc1
Egor <egor.garbar2014@gmail.com>
2025-09-17 12:27:12 +0300

    Горутины

--- server/internal/funcs/server_funcs.go
+++ server/internal/funcs/server_funcs.go
@@ -84,60 +84,66 @@ func (m *ClientManager) Run() {
 	for {
 		select {
 		case regClient := <-m.Register:
-			m.Mu.Lock()
-			if m.Clients[regClient.ID] != nil {
-				regClient.SendChan <- &proto.ChatMessage{
-					From:      "System",
-					To:        regClient.ID,
-					Type:      "system",
-					Content:   fmt.Sprintf("ID %v уже занят", regClient.ID),
-					Timestamp: time.Now().Unix(),
+			go func(m *ClientManager, regClient *Client) {
+				m.Mu.Lock()
+				if m.Clients[regClient.ID] != nil {
+					regClient.SendChan <- &proto.ChatMessage{
+						From:      "System",
+						To:        regClient.ID,
+						Type:      "system",
+						Content:   fmt.Sprintf("ID %v уже занят", regClient.ID),
+						Timestamp: time.Now().Unix(),
+					}
+					// time.Sleep(100 * time.Millisecond)
+					regClient.ID = ""
+					regClient.Cansel()
+				} else {
+					m.Clients[regClient.ID] = regClient
+					m.Broadcast <- &proto.ChatMessage{
+						From:      "System",
+						Type:      "system",
+						Content:   fmt.Sprintf("%v присоединился", regClient.ID),
+						Timestamp: time.Now().Unix(),
+					}
+					// time.Sleep(100 * time.Millisecond)
 				}
-				time.Sleep(100 * time.Millisecond)
-				regClient.ID = ""
-				regClient.Cansel()
-			} else {
-				m.Broadcast <- &proto.ChatMessage{
-					From:      "System",
-					Type:      "system",
-					Content:   fmt.Sprintf("%v присоединился", regClient.ID),
-					Timestamp: time.Now().Unix(),
-				}
-				time.Sleep(100 * time.Millisecond)
-				m.Clients[regClient.ID] = regClient
-			}
-			m.Mu.Unlock()
+				m.Mu.Unlock()
+			}(m, regClient)
 
 		case unregClient := <-m.Unregister:
-			m.Mu.Lock()
-			client, ok := m.Clients[unregClient.ID]
-			if ok {
-				delete(m.Clients, client.ID)
-				m.Broadcast <- &proto.ChatMessage{
-					From:      "System",
-					Type:      "system",
-					Content:   fmt.Sprintf("%v покинул чат", unregClient.ID),
-					Timestamp: time.Now().Unix(),
+			go func(m *ClientManager, unregClient *Client) {
+				m.Mu.Lock()
+				client, ok := m.Clients[unregClient.ID]
+				if ok {
+					delete(m.Clients, client.ID)
+					m.Broadcast <- &proto.ChatMessage{
+						From:      "System",
+						Type:      "system",
+						Content:   fmt.Sprintf("%v покинул чат", unregClient.ID),
+						Timestamp: time.Now().Unix(),
+					}
+					time.Sleep(100 * time.Millisecond)
 				}
-				time.Sleep(100 * time.Millisecond)
-			}
-			m.Mu.Unlock()
+				m.Mu.Unlock()
+			}(m, unregClient)
 
 		case msg := <-m.Broadcast:
-			m.Mu.Lock()
-			if to := msg.GetTo(); to == "" {
-				for _, client := range m.Clients {
-					if client.ID != msg.From {
+			go func(m *ClientManager, msg *proto.ChatMessage) {
+				m.Mu.Lock()
+				if to := msg.GetTo(); to == "" {
+					for _, client := range m.Clients {
+						if client.ID != msg.From {
+							client.SendChan <- msg
+						}
+					}
+				} else {
+					client, ok := m.Clients[to]
+					if ok {
 						client.SendChan <- msg
 					}
 				}
-			} else {
-				client, ok := m.Clients[to]
-				if ok {
-					client.SendChan <- msg
-				}
-			}
-			m.Mu.Unlock()
+				m.Mu.Unlock()
+			}(m, msg)
 		}
 	}
 }
